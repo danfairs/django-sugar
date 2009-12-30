5a748578b0e19a6102e75fa68a2160da80572c2a
Dan Fairs <dan@fezconsulting.com>
2009-12-30 18:32:22 +0000

    Make it easy to define alternate filters that don't just respond to <code>..</code> regexes, with tests.

--- sugar/templatetags/pygment_tags.py
+++ sugar/templatetags/pygment_tags.py
@@ -3,10 +3,9 @@ import re
 from pygments import lexers, formatters, highlight
 
 register = template.Library()
-regex = re.compile(r'<code>(.*?)</code>', re.DOTALL)
 
 @register.filter(name='pygmentize')
-def pygmentize(value):
+def pygmentize(value, arg='code'):
     '''
     Finds all <code></code> blocks in a text block and replaces it with 
     pygments-highlighted html semantics. It tries to guess the format of the 
@@ -21,22 +20,26 @@ def pygmentize(value):
     {% post.body|pygmentize %}
 
     '''
-    
-    try:
-        last_end = 0
-        to_return = ''
-        found = 0
-        for match_obj in regex.finditer(value):
-            code_string = match_obj.group(1)
-            try:
-                lexer = lexers.guess_lexer(code_string)
-            except ValueError:
-                lexer = lexers.PythonLexer()
-            pygmented_string = highlight(code_string, lexer, formatters.HtmlFormatter())
-            to_return = to_return + value[last_end:match_obj.start(1)] + pygmented_string
-            last_end = match_obj.end(1)
-            found = found + 1
-        to_return = to_return + value[last_end:]
-        return to_return
-    except:
-        return value
+    bits = arg.split(':')
+    if len(bits) == 1:
+        regex = '<%(element)s>(.*?)</%(element)s>' % {'element': bits[0].strip()}
+    else:
+        element, css_class = bits
+        regex = '<%(element)s\W*class=[\'|"]%(css_class)s[\'|"]\W*>(.*?)</%(element)s>' % {
+            'element': element.strip(),
+            'css_class': css_class.strip()
+        }
+
+    last_end = 0
+    to_return = ''
+    for match_obj in re.finditer(regex, value, re.DOTALL):
+        code_string = match_obj.group(1)
+        try:
+            lexer = lexers.guess_lexer(code_string)
+        except ValueError:
+            lexer = lexers.PythonLexer()
+        pygmented_string = highlight(code_string, lexer, formatters.HtmlFormatter())
+        to_return = to_return + value[last_end:match_obj.start(1)] + pygmented_string
+        last_end = match_obj.end(1)
+    return to_return + value[last_end:]
+
